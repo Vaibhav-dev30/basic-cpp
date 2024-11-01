a23faf072242d5ace2ca61b1700b776717cad64f
Vaibhav Kumar <kumarvaibhavvs16@gmail.com>
2024-11-01 15:45:57 +0530

    Update updationofnumbers.cpp

--- updationofnumbers.cpp
+++ updationofnumbers.cpp
@@ -1,9 +1,9 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//   int c = 5;
-//   cout<<c<<endl;
-//   cout<<"hi"<<endl;
-//   c = 9;
-//   cout<<c;
-// }
+#include <iostream>
+using namespace std;
+int main() {
+  int c = 5;
+  cout<<c<<endl;
+  cout<<"hi"<<endl;
+  c = 9;
+  cout<<c;
+}
