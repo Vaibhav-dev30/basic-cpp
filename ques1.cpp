209f47436aaea9c3f16a3e7b841c0148a2647be1
Vaibhav Kumar <kumarvaibhavvs16@gmail.com>
2024-11-01 21:30:49 +0530

    Update ques1.cpp

--- ques1.cpp
+++ ques1.cpp
@@ -1,11 +1,11 @@
-// #include <cmath>
-// #include <iostream>
-// using namespace std;
+#include <cmath>
+#include <iostream>
+using namespace std;
 
-// int main() {
-//   int num;
-//   cout << "enter a number: ";
-//   cin >> num;
-//   cout << abs(num) << endl;
-//   return 0;
-// }
+int main() {
+  int num;
+  cout << "enter a number: ";
+  cin >> num;
+  cout << abs(num) << endl;
+  return 0;
+}
