56212710a1da2a5de2390ebdaec6e98bc7b47610
Vaibhav Kumar <kumarvaibhavvs16@gmail.com>
2024-11-01 15:45:33 +0530

    Update sumofthenumbers.cpp

--- sumofthenumbers.cpp
+++ sumofthenumbers.cpp
@@ -1,10 +1,10 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//   int x,y;
-//   cout<<"enter the 1st number: ";
-//   cin>>x;
-//   cout<<"enter the 2nd number: ";
-//   cin>>y;
-//   cout<<"the sum of the numbers is: "<<x+y;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+  int x,y;
+  cout<<"enter the 1st number: ";
+  cin>>x;
+  cout<<"enter the 2nd number: ";
+  cin>>y;
+  cout<<"the sum of the numbers is: "<<x+y;
+}
