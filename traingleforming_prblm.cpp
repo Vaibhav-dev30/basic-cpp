a4102769261babb53bf457baa2d708cfb034f5e8
Vaibhav Kumar <kumarvaibhavvs16@gmail.com>
2024-11-01 21:29:58 +0530

    Update traingleforming_prblm.cpp

--- traingleforming_prblm.cpp
+++ traingleforming_prblm.cpp
@@ -1,12 +1,12 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
-//   int a,b,c;
-//   cout<<"enter three sides of a triangle: ";
-//   cin>>a>>b>>c;
-//   if(a+b>c && b+c>a && c)
-//     cout<<"triangle is formed";
-//   else
-//     cout<<"the triangle can't be formed";
-//   return 0;
-// }
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,c;
+  cout<<"enter three sides of a triangle: ";
+  cin>>a>>b>>c;
+  if(a+b>c && b+c>a && c)
+    cout<<"triangle is formed";
+  else
+    cout<<"the triangle can't be formed";
+  return 0;
+}
