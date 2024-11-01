14069cbcec9644f6b795cca02008da112e27f1bc
Vaibhav Kumar <kumarvaibhavvs16@gmail.com>
2024-11-01 15:44:18 +0530

    Update aritheeticoperator.cpp

--- aritheeticoperator.cpp
+++ aritheeticoperator.cpp
@@ -1,9 +1,9 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-// float x=5, y=8;
-//   cout<<x+y<<endl;
-//   cout<<x-y<<endl;
-//   cout<<x*y<<endl;
-//   cout<<x/y<<endl;
-// }
+#include <iostream>
+using namespace std;
+int main() {
+float x=5, y=8;
+  cout<<x+y<<endl;
+  cout<<x-y<<endl;
+  cout<<x*y<<endl;
+  cout<<x/y<<endl;
+}
