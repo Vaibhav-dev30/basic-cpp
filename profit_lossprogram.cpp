cadab7939470e65785041b96a8cab6d54ce0e714
Vaibhav Kumar <kumarvaibhavvs16@gmail.com>
2024-11-01 21:30:59 +0530

    Update profit_lossprogram.cpp

--- profit_lossprogram.cpp
+++ profit_lossprogram.cpp
@@ -1,22 +1,22 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main() {
-//   float cp, sp, profit, loss;
-//   cout << "Enter the Cost Price: ";
-//   cin >> cp;
-//   cout << "Enter the Selling Price: ";
-//   cin >> sp;
+int main() {
+  float cp, sp, profit, loss;
+  cout << "Enter the Cost Price: ";
+  cin >> cp;
+  cout << "Enter the Selling Price: ";
+  cin >> sp;
 
-//   if (sp > cp) {
-//     profit = sp - cp;
-//     cout << "yay! you've made Profit of = " << profit<< endl;
-//   } else if (cp > sp) {
-//     loss = cp - sp;
-//     cout << "oops you had Loss = " << loss << endl;
-//   } else {
-//     cout << "No profit, No loss" << endl;
-//   }
+  if (sp > cp) {
+    profit = sp - cp;
+    cout << "yay! you've made Profit of = " << profit<< endl;
+  } else if (cp > sp) {
+    loss = cp - sp;
+    cout << "oops you had Loss = " << loss << endl;
+  } else {
+    cout << "No profit, No loss" << endl;
+  }
 
-//   return 0;
-// }
+  return 0;
+}
